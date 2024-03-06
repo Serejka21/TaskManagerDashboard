476bf362d7a91ced516f5d7effc179ddf659ddef
Serhii Buryk <sergmarten21@gmail.com>
2024-03-06 15:32:33 +0200

    implement login/logout pages

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("dashboard.urls"), name="dashboard"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
