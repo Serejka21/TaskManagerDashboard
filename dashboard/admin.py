4e9398d95e072d95a8d08158c88dfac9590bb020
Serhii Buryk <sergmarten21@gmail.com>
2024-03-06 16:03:36 +0200

    add User model to admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from dashboard.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (("Additional info", {"fields": ("first_name", "last_name", "position",)}),)
+    )
