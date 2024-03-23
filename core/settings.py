62298c93c07dc7de4eac8d1a9871cc08111bc1ce
Serhii Buryk <sergmarten21@gmail.com>
2024-03-23 18:47:29 +0200

    remove insecure secret_key value from config for deploy

--- core/settings.py
+++ core/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
-                            'django-insecure-n0o8jdda7x$1!q_3s^6l^ukr9dh6u$#w(2#xsd^_lt%yhjxsa2')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
